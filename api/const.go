c7e0944c88de2b9f97ffc507e5a657b9cb749d2e
Garfield Freeman <gfreeman@paloaltonetworks.com>
2023-02-01 12:01:58 -0800

    moving constants to the api package

--- api/const.go
+++ api/const.go
@@ -1,5 +1,12 @@
 package api
 
+// Logging constants.
+const (
+	LogQuiet    = "quiet"
+	LogBasic    = "basic"
+	LogDetailed = "detailed"
+)
+
 const (
 	// Max value for limit in pagination queries.
 	MaxLimit = 2000
